62cc7fa05a767af6d5a0034e05eb1d4821bea6b3
Pablo Wilner <pablowilner@gmail.com>
2022-02-16 20:29:22 +0100

    challenge airbnb static

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Madrid flat for friends',
+  address: 'Madrid center',
+  description: 'Flat for friends',
+  price_per_night: 150,
+  number_of_guests: 6
+)
+
+Flat.create!(
+  name: 'Rome flat for couples',
+  address: 'Rome old city',
+  description: 'Flat for couples in rome',
+  price_per_night: 130,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Barcelona near the sea',
+  address: 'Barcelona gothic',
+  description: 'Flat for the family in Barcelona',
+  price_per_night: 80,
+  number_of_guests: 4
+)
